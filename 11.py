97e6f24ba57bed50b35fd8d2ad06c569ddf340d3
Lcw723 <C110156212@nkust.edu.tw>
2022-03-29 13:56:19 +0800

    期中

---
+++ 11.py
@@ -0,0 +1,62 @@
+md=input("請輸入月和日:").split(" ")
+m=int(md[0])
+d=int(md[1])
+if m==12 and d>=22 and d<=31:
+    print("星座為摩羯座")
+elif m==1 and d>=1 and d<=20:
+    print("星座為摩羯座")
+
+elif m==1 and d>=21 and d<=31:
+    print("星座為水瓶座")
+elif m==2 and d>=1 and d<=18:
+    print("星座為水瓶座")
+
+elif m==2 and d>=19 and d<=29:
+    print("星座為雙魚座")
+elif m==3 and d>=1 and d<=20:
+    print("星座為雙魚座")
+
+elif m==3 and d>=21 and d<=31:
+    print("星座為牡羊座")
+elif m==4 and d>=1 and d<=20:
+    print("星座為牡羊座")
+
+elif m==4 and d>=21 and d<=30:
+    print("星座為金牛座")
+elif m==5 and d>=1 and d<=21:
+    print("星座為金牛座")
+
+elif m==5 and d>=22 and d<=31:
+    print("星座為雙子座")
+elif m==6 and d>=1 and d<=21:
+    print("星座為雙子座")
+
+elif m==6 and d>=22 and d<=30:
+    print("星座為巨蟹座")
+elif m==7 and d>=1 and d<=22:
+    print("星座為巨蟹座")
+
+elif m==7 and d>=23 and d<=31:
+    print("星座為獅子座")
+elif m==8 and d>=1 and d<=23:
+    print("星座為獅子座")
+
+elif m==8 and d>=24 and d<=31:
+    print("星座為處女座")
+elif m==9 and d>=1 and d<=23:
+    print("星座為處女座")
+
+elif m==9 and d>=24 and d<=30:
+    print("星座為天秤座")
+elif m==10 and d>=1 and d<=23:
+    print("星座為天秤座")
+
+elif m==10 and d>=24 and d<=31:
+    print("星座為天蠍座")
+elif m==11 and d>=1 and d<=22:
+    print("星座為天蠍座")
+
+elif m==11 and d>=23 and d<=30:
+    print("星座為射手座")
+elif m==12 and d>=1 and d<=21:
+    print("星座為射手座")

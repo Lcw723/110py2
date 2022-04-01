f5cea8684f4c9ace3c0a838fa45d2866c9972a46
Lcw723 <C110156212@nkust.edu.tw>
2022-04-01 23:27:50 +0800

    期中

---
+++ 21.py
@@ -0,0 +1,8 @@
+sdata={}
+sdata["123"]=["123","Tom","DTGD"]
+sdata["456"]=["456","Cat","CSIE"]
+sdata["789"]=["789","Nana","ASIE"]
+sdata["321"]=["321","Lim","DBA"]
+sdata["654"]=["654","Won","FDD"]
+sid=input("輸入查詢學號為:")
+print(f"學生資料為:{sdata[sid]}")

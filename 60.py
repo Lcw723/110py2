ea270eb2df9c01ddce4c95220c46ab7a0f3b263d
Lcw723 <C110156212@nkust.edu.tw>
2022-04-09 15:21:35 +0800

    期中

---
+++ 60.py
@@ -0,0 +1,14 @@
+e=input("請輸入一段小寫英文:")
+en=[]
+for i in range(len(e)):
+    en.append(e[i])
+for i in en:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        e=e.replace(i,".")
+        e=e.replace(i,".")
+        e=e.replace(i,".")
+        e=e.replace(i,".")
+        e=e.replace(i,".")
+print(e)
+        
+        
